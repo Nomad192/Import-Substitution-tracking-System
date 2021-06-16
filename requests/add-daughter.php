4344fb5f125d3bb985816af625fa542e6e480175
Andrew <48153570+Nomad192@users.noreply.github.com>
2021-06-16 15:12:44 +0300

    Deleted the excess

--- requests/add-daughter.php
+++ requests/add-daughter.php
@@ -1,10 +1,4 @@
 <?php
-	/*echo shell_exec('%JAVA_HOME% tree/Main 0 ' . $_GET['id'] . ' ' . $_GET['name'] . ' ' . $_GET['parent']);
-	echo PHP_EOL;
-	echo PHP_EOL,'UTF-8',PHP_EOL;
-	shell_exec('chcp 65001');
-	echo shell_exec('%JAVA_HOME% tree/Main 0 ' . $_GET['id'] . ' ' . $_GET['name'] . ' ' . $_GET['parent']);
-	echo PHP_EOL;*/
 	shell_exec ('chcp 1251');
 	echo shell_exec('%JAVA_HOME% tree/Main 0 ' . $_GET['id'] . ' ' . iconv('windows-1251', 'UTF-8', $_GET['name']) . ' ' . $_GET['parent']);
 ?>
